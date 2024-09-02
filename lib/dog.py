7ed680d4c8c4487661873b950d4a1142becc74fa
Abdulmajid Aman Hussein <abdulmajid.aman.student@moringaschool.com>
2024-09-02 22:19:19 +0300

    Labs about instance methods and classes

--- lib/dog.py
+++ lib/dog.py
@@ -4,4 +4,14 @@ class Dog:
     # Class body goes here
 
     #Instance method definition
-    pass
+    def bark(self):
+        print('Woof!')
+
+    def sit(self):
+        print('The dog is sitting.')
+
+
+
+snoop = Dog()
+snoop.bark()  # Outputs: Woof!
+snoop.sit()
